bd3ff46fdea82d00949d9e955be303fa2b5bb5fc
Aleena Fatima Khalid <ainookhalid@gmail.com>
2024-03-28 08:23:08 +0500

    phase  2
    - phase 2 imp tracking script uploading to firebase
    
    Co-Authored-By: Kisaa Batool <114584799+KisaaBatool05@users.noreply.github.com>
    Co-Authored-By: Maaidah Arif Siddiqui <77479098+maaidah@users.noreply.github.com>

--- Assets/scripts/Improvement Tracking/GameDataLogger.cs
+++ Assets/scripts/Improvement Tracking/GameDataLogger.cs
@@ -1,3 +1,5 @@
+using Firebase.Auth;
+using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,45 +11,60 @@ public class GameDataLogger : MonoBehaviour
     public string gameTry; 
     private const string HINTS_USED_KEY = "Phase2Hints";
     private const string INCORRECT_PLAYS_KEY = "Phase2IncorrectCount";
+    private DatabaseReference databaseReference;
 
 
     private void Start()
     {
 
+        databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
         // Retrieve values from PlayerPrefs
-      
-      
+
+
         int hintsUsed = PlayerPrefs.GetInt(HINTS_USED_KEY);
         int incorrectPlays = PlayerPrefs.GetInt(INCORRECT_PLAYS_KEY);
-      
 
-        // Store values in ChildProfile's GameTry
-        
-        string childProfile = "Child1"; // Adjust as needed - idhar wo guardian ka laga dena
-        StoreGameData(childProfile, gameTry, hintsUsed, incorrectPlays);
+
+        AddAccuracyToDB(hintsUsed, incorrectPlays);
+
 
         // Reset PlayerPrefs
         ResetPlayerPrefs();
     }
 
- 
-
-    private void StoreGameData(string childProfile, string gameTry, float hintsUsed, int incorrectPlays)
+    public void AddAccuracyToDB(int hintsUsed, int incorrectPlays)
     {
-        // Calculate accuracy
         float accuracy = CalculateAccuracy(hintsUsed, incorrectPlays);
+        string userId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
+        string childId = PlayerPrefs.GetString("LoggedInChild");
 
-        // Store values in ChildProfile's GameTry
-        PlayerPrefs.SetFloat(childProfile + "_Game" + gameTry + "_" + HINTS_USED_KEY, hintsUsed);
-        PlayerPrefs.SetInt(childProfile + "_Game" + gameTry + "_" + INCORRECT_PLAYS_KEY, incorrectPlays);
-        PlayerPrefs.SetFloat(childProfile + "_Game" + gameTry + "_Accuracy", accuracy);
+        if (!string.IsNullOrEmpty(userId) && !string.IsNullOrEmpty(childId))
+        {
+            // Construct the path to update the avatar value in the database
+            string path = $"childProfiles/{userId}/profiles/{childId}/ImprovementTracking02/";
 
-        Debug.Log("Hints: " + PlayerPrefs.GetInt(HINTS_USED_KEY));
-        Debug.Log("Incorrect: "+ PlayerPrefs.GetInt(INCORRECT_PLAYS_KEY));
-        Debug.Log("Accuracy: " + PlayerPrefs.GetFloat(childProfile + "_Game" + gameTry + "_Accuracy"));
+            DatabaseReference impRef = databaseReference.Child(path).Push();
 
+            // Update the avatar value in the Firebase Realtime Database
+            impRef.SetValueAsync(accuracy).ContinueWith(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    Debug.Log("accuracy updated successfully!");
+                }
+                else if (task.IsFaulted)
+                {
+                    Debug.LogError("Failed to update accuracy: " + task.Exception);
+                }
+            });
+        }
+        else
+        {
+            Debug.LogWarning("accuracy name is empty!");
+        }
 
     }
+
 
     private float CalculateAccuracy(float hintsUsed, int incorrectPlays)
     {
